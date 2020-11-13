c6a594bb564c430d29c08da853283edb7ab26186
Satya Sharma <71933842+SatyaSharma73@users.noreply.github.com>
2020-11-13 19:41:52 +0530

    Update Covid_Code.py

--- Covid_Code.py
+++ Covid_Code.py
@@ -1,1 +1,14 @@
+from covid import Covid
 
+covid = Covid()
+# printing data for the world
+print("Total active cases in world:", covid.get_total_active_cases())
+print("Total recovered cases in world:", covid.get_total_recovered())
+print("Total deaths in world:", covid.get_total_deaths())
+# getting data according to country name
+# data will be stored as a dictionary
+
+cases = covid.get_status_by_country_name("INDIA") 
+# printing country's data using for loop
+for x in cases:
+    print(x, ":", cases[x])
